b52c1f6af3d7cb077d00c5296d17af865128aee2
Matthew Abrahamson <mra.abrahamson@gmail.com>
2019-01-09 22:21:20 -0800

    working spotify module

--- liri.js
+++ liri.js
@@ -1,167 +1,165 @@
-	// Node module imports needed to run the functions
-	var fs = require("fs"); //reads and writes files
-	var request = require("request");
-	var keys = require("./keys.js");
-	var axios = require("axios");
-	var spotify = require("node-spotify-api");
-	var liriArgument = process.argv[2];
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
+require("dotenv").config();
 
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
 
-// ---------------------------------------------------------------------------------------------------------------
+// Import the API keys
+var keys = require("./keys");
 
+// Import the axios npm package.
+var axios = require("axios");
 
-	// Possible commands for this liri app
+// Import the moment npm package.
+var moment = require("moment");
 
-	switch(liriArgument) {
-		case "bands-in-town": bandsInTown(); break;
-		case "spotify-this-song": spotifyThisSong(); break;
-		case "movie-this": movieThis(); break;
-		case "do-what-it-says": doWhatItSays(); break;
-		// Instructions displayed in terminal to the user
-		default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
-			"1. bands-in-town 'any touring band name' " +"\r\n"+
-			"2. spotify-this-song 'any song name' "+"\r\n"+
-			"3. movie-this 'any movie name' "+"\r\n"+
-			"4. do-what-it-says."+"\r\n"+
-			"Be sure to put the movie or song name in quotation marks if it's more than one word.");
-	};
+// Import the FS package for read/write.
+var fs = require("fs");
 
+// Initialize the spotify API client using our client id and secret
+var spotify = new Spotify(keys.spotify);
 
-// ---------------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// =====================================
 
+// Helper function that gets the artist name
+var getArtistNames = function(artist) {
+  return artist.name;
+};
 
-// Functions
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
 
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
 
-	// Bands in Town function, uses the axios module to call the bands in town api
-	function bandsInTown(bandName) {
-		var bandName = process.argv[3];
-		if(!bandName){
-			bandName = "Goo Goo Dolls";
-		}
-		params = bandName;
-		axios.search({ type: "event", query: params }, function(err, data) {
-			if(!err){
-				var songInfo = data.tracks.items;
-				for (var i = 0; i < 5; i++) {
-					if (songInfo[i] != undefined) {
-						var bandsInTownResults =
-						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
-						"Song: " + songInfo[i].name + "\r\n" +
-						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
-						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
-						"------------------------------ " + i + " ------------------------------" + "\r\n";
-						console.log(bandsInTownResults);
-						log(bandsInTownResults); // calling log function
-					}
-				}
-			}	else {
-				console.log("Error :"+ err);
-				return;
-			}
-		});
-	};
+      var songs = data.tracks.items;
 
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
 
-	// Movie function, uses the Request module to call the OMDB api
-	function movieThis(){
-		var movie = process.argv[3];
-		if(!movie){
-			movie = "mr nobody";
-		}
-		params = movie
-		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var movieObject = JSON.parse(body);
-				//console.log(movieObject); // Show the text in the terminal
-				var movieResults =
-				"------------------------------ begin ------------------------------" + "\r\n"
-				"Title: " + movieObject.Title+"\r\n"+
-				"Year: " + movieObject.Year+"\r\n"+
-				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
-				"Country: " + movieObject.Country+"\r\n"+
-				"Language: " + movieObject.Language+"\r\n"+
-				"Plot: " + movieObject.Plot+"\r\n"+
-				"Actors: " + movieObject.Actors+"\r\n"+
-				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
-				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
-				"------------------------------ fin ------------------------------" + "\r\n";
-				console.log(movieResults);
-				log(movieResults); // calling log function
-			} else {
-				console.log("Error :"+ error);
-				return;
-			}
-		});
-	};
+var getMyBands = function(artist) {
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-	// Spotify function, uses the Spotify module to call the Spotify api
+  axios.get(queryURL).then(
+    function(response) {
+      var jsonData = response.data;
 
+      if (!jsonData.length) {
+        console.log("No results found for " + artist);
+        return;
+      }
 
-	var spotify = new Spotify({
-		id: <your spotify client id>,
-		secret: <your spotify client secret>
-	  });
-	   
-	  var songName = process.argv[3];
-		if(!songName){
-			songName = "What's my age again";
-		}
-		params = songName;
+      console.log("Upcoming concerts for " + artist + ":");
 
-	  spotify.search({ type: 'track', query: 'params' }, function(err, data) {
-		if (err) {
-		  return console.log('Error occurred: ' + err);
-		}
-	   
-	  console.log(data); 
-	  });
+      for (var i = 0; i < jsonData.length; i++) {
+        var show = jsonData[i];
 
+        // Print data about each concert
+        // If a concert doesn't have a region, display the country instead
+        // Use moment to format the date
+        console.log(
+          show.venue.city +
+            "," +
+            (show.venue.region || show.venue.country) +
+            " at " +
+            show.venue.name +
+            " " +
+            moment(show.datetime).format("MM/DD/YYYY")
+        );
+      }
+    }
+  );
+};
 
-	// function spotifyThisSong(songName) {
-		// var songName = process.argv[3];
-		// if(!songName){
-		// 	songName = "What's my age again";
-		// }
-		// params = songName;
-	// 	spotify.search({ type: "track", query: params }, function(err, data) {
-	// 		if(!err){
-	// 			var songInfo = data.tracks.items;
-	// 			for (var i = 0; i < 5; i++) {
-	// 				if (songInfo[i] != undefined) {
-	// 					var spotifyResults =
-	// 					"Artist: " + songInfo[i].artists[0].name + "\r\n" +
-	// 					"Song: " + songInfo[i].name + "\r\n" +
-	// 					"Album the song is from: " + songInfo[i].album.name + "\r\n" +
-	// 					"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
-	// 					"------------------------------ " + i + " ------------------------------" + "\r\n";
-	// 					console.log(spotifyResults);
-	// 					log(spotifyResults); // calling log function
-	// 				}
-	// 			}
-	// 		}	else {
-	// 			console.log("Error :"+ err);
-	// 			return;
-	// 		}
-	// 	});
-	// };
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
 
-	// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
-	function doWhatItSays() {
-		fs.readFile("random.txt", "utf8", function(error, data){
-			if (!error) {
-				doWhatItSaysResults = data.split(",");
-				spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
-			} else {
-				console.log("Error occurred" + error);
-			}
-		});
-	};
-	// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
-	function log(logResults) {
-	  fs.appendFile("log.txt", logResults, (error) => {
-	    if(error) {
-	      throw error;
-	    }
-	  });
-	}
+  var urlHit =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  axios.get(urlHit).then(
+    function(response) {
+      var jsonData = response.data;
+
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+    }
+  );
+};
+
+// Function for running a command based on text file
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+
+// Function for determining which command is executed
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+  case "concert-this":
+    getMyBands(functionData);
+    break;
+  case "spotify-this-song":
+    getMeSpotify(functionData);
+    break;
+  case "movie-this":
+    getMeMovie(functionData);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+  }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
